2007651e1732a0d356caa6a5b3cea4b176367b8e
Vincent Monet <vincent@dacker.co>
2019-10-31 10:48:41 +0100

    init

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='pydatasource',
-    version='0.0.4',
+    version='0.0.5',
     description='Easily manage a dataflow',
     long_description=readme,
     author='Dacker',
