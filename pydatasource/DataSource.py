fe917a5556e68fcdb446fe2bc9e51c2eb94093c4
Vincent Monet <vincent@dacker.co>
2020-01-15 11:31:00 +0100

    doc

--- pydatasource/DataSource.py
+++ pydatasource/DataSource.py
@@ -16,10 +16,10 @@ def _doc_treat_query(filled_query, schema_name, table_name):
         n = aa[0]
         if '.' in n:
             all_tuple.append({'schema_output': schema_name, 'table_output': table_name,
-                              'schema_input': n.split('.')[0],
-                              'table_input': n.split('.')[1],
+                              'schema_input': n.split('.')[0].replace(')', ''),
+                              'table_input': n.split('.')[1].replace(')', ''),
                               "schema_table_output": schema_name + '.' + table_name,
-                              "schema_table_input": n})
+                              "schema_table_input": n.replace(')','')})
     return all_tuple
 
 
