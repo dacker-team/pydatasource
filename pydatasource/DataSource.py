e804fc4d714ad706e081820d4a6600bba3c9d948
VIncent Monet <vincentmonet93@gmail.com>
2021-04-23 14:54:12 +0200

    update

--- pydatasource/DataSource.py
+++ pydatasource/DataSource.py
@@ -76,6 +76,14 @@ def date_range_params(period_config, comparison, period, reference_date):
         start_date = (today + relativedelta(days=-14)).strftime("%Y-%m-%d")
         end_date = (today + relativedelta(days=-7)).strftime("%Y-%m-%d")
 
+    elif period_config == "l30d":
+        start_date = (today + relativedelta(days=-30)).strftime("%Y-%m-%d")
+        end_date = today.strftime("%Y-%m-%d")
+
+    elif period_config == "previous_l30d":
+        start_date = (today + relativedelta(days=-60)).strftime("%Y-%m-%d")
+        end_date = (today + relativedelta(days=-30)).strftime("%Y-%m-%d")
+
     elif period_config == "ly":
         start_date = (today + relativedelta(years=-1)).strftime("%Y-01-01")
         end_date = today.strftime("%Y-01-01")
