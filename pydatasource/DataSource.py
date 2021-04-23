1e2b727a8910e56753c0928d11ce73052c8a62b5
VIncent Monet <vincentmonet93@gmail.com>
2021-04-23 14:23:26 +0200

    new period

--- pydatasource/DataSource.py
+++ pydatasource/DataSource.py
@@ -76,6 +76,13 @@ def date_range_params(period_config, comparison, period, reference_date):
         start_date = (today + relativedelta(days=-14)).strftime("%Y-%m-%d")
         end_date = (today + relativedelta(days=-7)).strftime("%Y-%m-%d")
 
+    elif period_config == "ly":
+        start_date = (today + relativedelta(years=-1)).strftime("%Y-01-01")
+        end_date = today.strftime("%Y-01-01")
+
+    elif period_config == "month_ly":
+        start_date = (today + relativedelta(years=-1)).strftime("%Y-%m-01")
+        end_date = (today + relativedelta(years=-1) + relativedelta(month=1)).strftime("%Y-%m-01")
 
 
     else:
