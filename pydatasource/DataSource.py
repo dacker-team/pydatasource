0ea8e9fa337124c3ea7967d04ff9a54928d7252c
VIncent Monet <vincentmonet93@gmail.com>
2021-05-27 11:27:24 +0200

    Cascade

--- pydatasource/DataSource.py
+++ pydatasource/DataSource.py
@@ -291,13 +291,7 @@ class DataSource:
                 );
                 """ % query_string
             elif query_config.get("drop_clause") == "cascade":
-                query_string = """
-                drop table if exists {{ table_name }} CASCADE ;
-                create table {{ table_name }} as (
-                %s
-                );
-                """ % query_string
-                query_string = self.dbstream.build_pydatasource_table(query_string)
+                query_string = self.dbstream.build_pydatasource_table_cascade(query_string)
             else:
                 query_string = self.dbstream.build_pydatasource_table(query_string)
         template = self.jinja_env.from_string(query_string)
